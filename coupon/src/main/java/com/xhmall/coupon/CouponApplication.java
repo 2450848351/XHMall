697321e1d03583fa06e569ef699d0c4e6bf324ff
Lihuan <2450848351@qq.com>
2023-06-01 20:02:01 +0800

    项目初始结构2

--- coupon/src/main/java/com/xhmall/coupon/CouponApplication.java
+++ coupon/src/main/java/com/xhmall/coupon/CouponApplication.java
@@ -1,8 +1,10 @@
 package com.xhmall.coupon;
 
+import org.apache.ibatis.annotations.Mapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@Mapper
 @SpringBootApplication
 public class CouponApplication {
 
--- product/src/test/java/com/xhmall/product/ProductApplicationTests.java
+++ product/src/test/java/com/xhmall/product/ProductApplicationTests.java
@@ -1,13 +1,27 @@
 package com.xhmall.product;
 
-import org.junit.jupiter.api.Test;
+import com.xhmall.product.entity.BrandEntity;
+import com.xhmall.product.service.BrandService;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+@RunWith(SpringRunner.class)
 @SpringBootTest
-class ProductApplicationTests {
+public class ProductApplicationTests {
 
+    @Autowired
+    BrandService brandService;
     @Test
-    void contextLoads() {
+    public void contextLoads() {
+        BrandEntity brandEntity = new BrandEntity();
+//        brandEntity.setName("华为");
+        brandEntity.setBrandId(1L);
+        brandEntity.setDescript("中华有为");
+        brandService.updateById(brandEntity);
+        System.out.println("保存成功...");
     }
 
 }
